71e0b99ed236602ef1a523bc035e914b558749a2
sagyzdop <sagyzdop+github@gmail.com>
2025-03-05 17:46:21 +0500

    everything up to chapter 3.3

---
+++ cmd/web/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"flag"
+	"log/slog" // New import
+	"net/http"
+	"os" // New import
+)
+
+func main() {
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	flag.Parse()
+
+	// Use the slog.New() function to initialize a new structured logger, which
+	// writes to the standard out stream and uses the default settings.
+	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+		Level:     slog.LevelDebug,
+		AddSource: true,
+	}))
+
+	mux := http.NewServeMux()
+
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+
+	mux.HandleFunc("GET /{$}", home)
+	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
+	mux.HandleFunc("GET /snippet/create", snippetCreate)
+	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
+
+	// Use the Info() method to log the starting server message at Info severity
+	// (along with the listen address as an attribute).
+	logger.Info("starting server at http://localhost", "addr", *addr)
+
+	err := http.ListenAndServe(*addr, mux)
+	// And we also use the Error() method to log any error message returned by
+	// http.ListenAndServe() at Error severity (with no additional attributes),
+	// and then call os.Exit(1) to terminate the application with exit code 1.
+	logger.Error(err.Error())
+	os.Exit(1)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module snippetbox.sagyzdop.com
+
+go 1.23.5
